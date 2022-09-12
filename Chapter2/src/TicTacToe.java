85be023f012f338572d2e73746a6df5104c0f47b
16066017 <16066017@L815WD220029.edu.cbe.ab.ca>
2022-09-12 13:47:42 -0600

    1:47

--- Chapter2/src/TicTacToe.java
+++ Chapter2/src/TicTacToe.java
@@ -2,7 +2,17 @@
 public class TicTacToe {
 
 	public static void main(String[] args) {
-		
+		System.out.println("   |   |   ");
+		System.out.println("   |   |   ");
+		System.out.println("   |   |   ");
+		System.out.println("-----------");
+		System.out.println("   |   |   ");
+		System.out.println("   | x |   ");
+		System.out.println("   |   |   ");
+		System.out.println("-----------");
+		System.out.println("   |   |   ");
+		System.out.println("   |   |   ");
+		System.out.println("   |   |   ");
 	}
 
 }
