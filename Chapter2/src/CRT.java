22db67c27104a727f2763e00b0d1b3147b6dcbe9
16066017 <16066017@L815WD220005.edu.cbe.ab.ca>
2022-09-14 13:55:11 -0600

    1:55

---
+++ Chapter2/src/CRT.java
@@ -0,0 +1,36 @@
+
+public class CRT {
+	
+	/*
+
+	Program: CRT.java          Last Date of this Revision: September 14, 2022
+
+	Purpose: An application that outputs my initials.
+
+	Author: Junaid, 
+	School: CHHS
+	Course: CS20
+	 
+
+	*/
+
+	public static void main(String[] args) {
+		
+		/*
+		 3. Calculates test averages
+		 
+		 4. Displays the average of a set of test scores
+		 
+		 5. package gradeCalculator;
+		 
+		 6. public class AverageScore{
+		 }
+		 
+		 7.Source code is typed by a programmer and bytecode is compiled source code by Java
+		 
+		 12. Print() displays the data and leaves the insertion point at the end of the line but println() displays the data and moves the insertion point to the next line
+		 */
+
+	}
+
+}
