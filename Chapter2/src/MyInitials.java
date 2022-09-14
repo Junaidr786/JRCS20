6c5521302078d1c81f30b2c91386fc28542b2a4e
16066017 <16066017@L815WD220005.edu.cbe.ab.ca>
2022-09-14 13:36:48 -0600

    1:36

---
+++ Chapter2/src/MyInitials.java
@@ -0,0 +1,28 @@
+
+public class MyInitials {
+	
+	/*
+
+	Program: MyInitials.java          Last Date of this Revision: September 14, 2022
+
+	Purpose: An application that outputs my initials.
+
+	Author: Junaid, 
+	School: CHHS
+	Course: CS20
+	 
+
+	*/
+
+	public static void main(String[] args) {
+		
+		System.out.println("J R");
+		
+	}
+	
+	/* Screen Dump
+	 
+	J R
+	 */
+
+}
