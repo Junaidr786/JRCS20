d2801cda2df7f14ff72fff8722d70265428bbcb2
16066017 <16066017@L815WD220005.edu.cbe.ab.ca>
2022-09-14 13:28:28 -0600

    1:28

--- Chapter2/src/TicTacToe.java
+++ Chapter2/src/TicTacToe.java
@@ -1,5 +1,18 @@
 
 public class TicTacToe {
+	
+	/*
+
+	Program: TicTacToe.java          Last Date of this Revision: September 12, 2022
+
+	Purpose: An application that outputs a tictactoe board
+
+	Author: Junaid, 
+	School: CHHS
+	Course: CS20
+	 
+
+	*/
 
 	public static void main(String[] args) {
 		System.out.println("   |   |   ");
@@ -14,5 +27,21 @@ public class TicTacToe {
 		System.out.println("   |   |   ");
 		System.out.println("   |   |   ");
 	}
+	
+	/* Screen Dump
+	 
+		   |   |   
+		   |   |   
+		   |   |   
+		-----------
+		   |   |   
+		   | x |   
+		   |   |   
+		-----------
+		   |   |   
+		   |   |   
+		   |   |   
 
+		
+	 */
 }
